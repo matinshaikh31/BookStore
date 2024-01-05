9d993c969b5a5602272c560cd1ea10a12061df98
Umerahmed Shaikh <matinshaikh79070@gmail.com>
2024-01-05 15:18:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+
+const addToCart = document.getElementById("add-to-cart");
+const cartMessage = document.getElementById("cart-message");
+
+addToCart.addEventListener("click", () => {
+  cartMessage.style.display = "block";
+});
